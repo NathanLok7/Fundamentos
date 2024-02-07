4fc85a29e02214cb7e7ca142b0152447d2aacfdf
Nathan Lokier <91803058+NathanLok7@users.noreply.github.com>
2024-02-07 10:11:03 -0600

    Add files via upload

--- ejer1.c
+++ ejer1.c
@@ -2,39 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
-#include <signal.h>
-
-void sig_handler(int signo) {
-    if (signo == SIGTERM)
-        printf("Proceso hijo recibió SIGTERM\n");
-}
 
 int main() {
-
-    pid_t pid;
-    pid = fork();
-
-    if (pid == -1) {
-        perror("Error al crear proceso hijo");
-        exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-        // Si existe el padre se ejecuta el codigo del hijo
-        printf("Proceso hijo iniciado. PID: %d\n", getpid());
-        signal(SIGTERM, sig_handler); // Manejar la señal SIGTERM
-        pause();
-        printf("Proceso hijo finalizado.\n");
-    } else {
-        // Código del proceso padre
-        printf("Proceso padre iniciado. PID: %d\n", getpid());
-        sleep(5); // Esperar 5 segundos
-        printf("Matando al proceso hijo. PID: %d\n", pid);
-        kill(pid, SIGTERM); // Enviar señal SIGTERM al proceso hijo
-        wait(NULL); // Esperar a que el proceso hijo termine
-        printf("Proceso padre finalizado.\n");
+    pid_t p;
+    p = fork();
+    
+    if(p < 0) {
+        perror("Error al crear el proceso hijo. \n");
+    } 
+    else if(p == 0) {
+        for(int i = 0; i < 10; i++) {
+            printf("Soy el hijo\n");
+            sleep(1); 
+        }
+    } 
+    else {
+        for(int i = 0; i < 10; i++) {
+            printf("Soy el padre\n");
+            sleep(1); 
+        }
+        wait(NULL);
+        printf("Mi proceso hijo ya ha terminado \n");
     }
-
     return 0;
 }
-
