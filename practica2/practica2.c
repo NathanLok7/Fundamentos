0934af8bd5f3d3d71b3caf212c5df4da2552b972
jpcortezn <90473293+jpcortezn@users.noreply.github.com>
2024-02-25 10:25:05 -0600

    Update practica2.c

--- practica2/practica2.c
+++ practica2/practica2.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <pthread.h>
-#include <sys/wait.h>
 #include <sys/time.h>
 
 #define MAX_THREADS 4
@@ -17,26 +15,23 @@ int is_prime(int num) {
 }
 
 void* count_primes(void* arg) {
-    int start = *((int*)arg);
-    int end = start + 2500000; // Each thread processes 2,500,000 numbers
-    int* count = malloc(sizeof(int));
-    *count = 0;
+    int* range = (int*)arg;
+    int start = range[0];
+    int end = range[1];
+    int count = 0;
 
-    for (int i = start; i < end; ++i) {
+    for (int i = start; i <= end; ++i) {
         if (is_prime(i)) {
-            (*count)++;
+            count++;
         }
     }
 
-    return count;
+    int* result = malloc(sizeof(int));
+    *result = count;
+    return result;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 1) {
-        printf("Usage: %s\n", argv[0]);
-        return 1;
-    }
-
+int main() {
     int start, end;
     printf("Enter the range (start end): ");
     scanf("%d %d", &start, &end);
@@ -46,18 +41,29 @@ int main(int argc, char* argv[]) {
 
     int total_primes = 0;
     pthread_t threads[MAX_THREADS];
-    int thread_args[MAX_THREADS];
+    int thread_args[MAX_THREADS][2];
+
+    int range_size = (end - start + 1) / MAX_THREADS;
+    int remaining = (end - start + 1) % MAX_THREADS;
+    int current_start = start;
+    int current_end;
 
     for (int i = 0; i < MAX_THREADS; ++i) {
-        thread_args[i] = start + i * 2500000;
-        pthread_create(&threads[i], NULL, count_primes, (void*)&thread_args[i]);
+        current_end = current_start + range_size - 1;
+        if (i < remaining) {
+            current_end++;
+        }
+        thread_args[i][0] = current_start;
+        thread_args[i][1] = current_end;
+        pthread_create(&threads[i], NULL, count_primes, (void*)thread_args[i]);
+        current_start = current_end + 1;
     }
 
     for (int i = 0; i < MAX_THREADS; ++i) {
         int* thread_count;
         pthread_join(threads[i], (void**)&thread_count);
         total_primes += *thread_count;
-        free(thread_count); // Liberar la memoria asignada en count_primes
+        free(thread_count);
     }
 
     gettimeofday(&end_time, NULL);
