0463ff2aab5733de23823bb6eeb5aaf036e2300b
Nathan Lokier <91803058+NathanLok7@users.noreply.github.com>
2024-04-07 22:56:23 -0600

    Update prueba.c

--- prueba.c
+++ prueba.c
@@ -1,4 +1,4 @@
-/* 
+/*
 Para compilar incluir la librería m (matemáticas)
 Ejemplo:
 gcc -o mercator mercator.c -lm
@@ -20,57 +20,62 @@ int *proc_count;
 int *start_all;
 double *res;
 
-double get_member(int n, double x)
-{
+double get_member(int n, double x) {
     int i;
     double numerator = 1;
-    for(i = 0; i < n; i++)
+    for (i = 0; i < n; i++) {
         numerator = numerator * x;
-    if (n % 2 == 0)
+    }
+    if (n % 2 == 0) {
         return (-numerator / n);
-    else
+    } else {
         return numerator / n;
+    }
 }
 
-void proc(int proc_num)
-{
+void proc(int proc_num) {
     int i;
-    while (!(*start_all))
-        ;
+    while (!(*start_all));
     sums[proc_num] = 0;
-    for (i = proc_num; i < SERIES_MEMBER_COUNT; i += NPROCS)
+    for (i = proc_num; i < SERIES_MEMBER_COUNT; i += NPROCS) {
         sums[proc_num] += get_member(i + 1, x);
+    }
     (*proc_count)++;
     exit(0);
 }
 
-void master_proc()
-{
+void master_proc() {
     int i;
     sleep(1);
     *start_all = 1;
-    while (*proc_count != NPROCS)
-        ; // espera ocupada hasta que todos los subprocesos hayan terminado el cálculo de las sumas parciales
+    while (*proc_count != NPROCS) {} // busy wait until all threads are done
+                                    // with computation of partial sums
     *res = 0;
-    for (i = 0; i < NPROCS; i++)
+    for (i = 0; i < NPROCS; i++) {
         *res += sums[i];
+    }
     exit(0);
 }
 
-int main()
-{
-    int i, p, shmid;
-    void *shmstart;
+int main() {
+    int *threadIdPtr;
+    long long start_ts;
+    long long stop_ts;
+    long long elapsed_time;
+    long lElapsedTime;
     struct timeval ts;
-    long long start_ts, stop_ts, elapsed_time;
+    int i;
+    int p;
+    int shmid;
+    void *shmstart;
 
     shmid = shmget(0x1234, NPROCS * sizeof(double) + 2 * sizeof(int), 0666 | IPC_CREAT);
     shmstart = shmat(shmid, NULL, 0);
 
-    sums = (double *)shmstart;
-    proc_count = (int *)(sums + NPROCS);
-    start_all = proc_count + 1;
-    res = (double *)(start_all + 1);
+    sums = shmstart;
+    proc_count = shmstart + NPROCS * sizeof(double);
+    start_all = shmstart + NPROCS * sizeof(double) + sizeof(int);
+    res = shmstart + NPROCS * sizeof(double) + 2 * sizeof(int);
 
     *proc_count = 0;
     *start_all = 0;
@@ -78,27 +83,28 @@ int main()
     gettimeofday(&ts, NULL);
     start_ts = ts.tv_sec; // Tiempo inicial
 
-    for (i = 0; i < NPROCS; i++)
-    {
+    for (i = 0; i < NPROCS; i++) {
         p = fork();
-        if (p == 0)
+        if (p == 0) {
             proc(i);
+        }
     }
 
     p = fork();
-    if (p == 0)
+    if (p == 0) {
         master_proc();
+    }
 
     printf("El recuento de ln(1 + x) miembros de la serie de Mercator es %d\n", SERIES_MEMBER_COUNT);
     printf("El valor del argumento x es %f\n", (double)x);
 
-    for (i = 0; i < NPROCS + 1; i++)
+    for (int i = 0; i < NPROCS + 1; i++) {
         wait(NULL);
+    }
 
     gettimeofday(&ts, NULL);
     stop_ts = ts.tv_sec; // Tiempo final
     elapsed_time = stop_ts - start_ts;
-
     printf("Tiempo = %lld segundos\n", elapsed_time);
     printf("El resultado es %10.8f\n", *res);
     printf("Llamando a la función ln(1 + %f) = %10.8f\n", x, log(1 + x));
